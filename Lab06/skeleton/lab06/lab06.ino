aa971f0981343551ac0b5b73ea65a963aba895b5
gibernas <gibernas@ethz.ch>
2018-05-07 14:31:30 +0200

    v1.1

---
+++ Lab06/skeleton/lab06/lab06.ino
@@ -0,0 +1,192 @@
+/*
+IRM Lab06 : Closed-loop control
+
+*/
+
+// Include the required libraries here (already done for you)
+//#include <string.h>
+#include <Adafruit_MotorShield.h>
+#include <string.h>
+#include <Wire.h>
+//#include "utility/Adafruit_PWMServoDriver.h"
+
+
+// define your global variables here
+int stepsPerRevolution = 200;
+int portMotor1;
+int portMotor2;
+char direction[20];
+// create a new object of Adafruit_MotorShield, you can call it AFMS
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// create one instance for each motor, call them myMotor1 and myMotor2
+Adafruit_StepperMotor *myMotor1 = AFMS.getStepper(stepsPerRevolution, portMotor1);
+Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(stepsPerRevolution, portMotor2);
+//
+
+// initialize pinouts for limit switches
+int switch1 = 6;
+int switch2 = 7;
+int switch3 = 4;
+int switch4 = 5;
+
+
+// initialize step size (Number of steps in each iteration)
+int stepSize = 20;
+
+// Functions
+
+void setup() {
+
+  // Start the serial communication at 115200 baud rate
+  Serial.begin(115200);
+  // Set serial communication timeout to 10
+  Serial.setTimeout(10);
+
+  // Start the Adafruit Motor Shield and set the maximum speed of the stepper
+  AFMS.begin();
+
+  // Set the input pins
+  pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
+  pinMode(switch3, INPUT);
+  pinMode(switch4, INPUT);
+}
+
+
+int move_steps (int steps, int dir, int motor) {
+   int switch_check;
+
+  // Check the motor and direction of movement, and set the limiting switch accordingly
+  switch (motor) {
+
+    case 1:
+    switch (dir) {
+
+      case 1:
+      //strncpy( direction, "FORWARD", sizeof(direction));
+      switch_check = switch2;
+      break;
+      case 2:
+      switch_check = switch1;
+      break;
+    }
+
+    case 2:
+    switch (dir) {
+      case 1:
+      switch_check = switch4;
+      break;
+      case 2:
+      switch_check = switch3;
+      break;
+    }
+
+    break;
+
+  }
+
+  // Limit the total number of steps to 999
+  if (steps > 999) steps = 999;
+
+  // Create a loop, which is executed if steps > 0 and the limit switch has not been reached,
+  // in the loop, move the desired motor in small steps (stepsize). Execute the loop until you moved
+  // the whole distance
+  while (steps > 0 && digitalRead(switch_check) == LOW){
+    if (motor == 1 ) myMotor1->step(stepSize, dir, SINGLE);
+    if (motor == 2 ) myMotor2->step(stepSize, dir, SINGLE);
+    steps -= stepSize;
+  }
+
+  // After running the loop, return ASCII for the switches
+
+    // If switch 1 is pressed, return '1'
+   if (digitalRead(switch1) == HIGH) return 49;
+    // If switch 2 is pressed, return '2'
+   else if(digitalRead(switch2) == HIGH) return 50;
+    // If switch 3 is pressed, return '3'
+   else if(digitalRead(switch3) == HIGH) return 51;
+    // If switch 4 is pressed, return '4'
+   else if(digitalRead(switch4) == HIGH) return 52;
+    // Else, return '0'
+   else return 48;
+}
+
+
+void loop() {
+  // Initialize parameters
+  char command[50];
+  char check;
+  byte read_check;
+  int flag = 1; //if 1 command is proper, else not
+  int steps = 0;
+  int motor = 0;
+  int direction = 0;
+
+  // Check if there is a command on the serial port
+  if (Serial.available() > 0)
+  {
+    read_check = Serial.readBytes(command,20);
+    // If the command is not 5 bytes long, discard it
+    if ((int)read_check == 5)
+    {
+      command[5] = '\0';
+      check = '0';
+    }
+    else
+    {
+      command[0] = '\0';
+      check = '5';
+      Serial.print(check);
+    }
+  }
+
+  // Proper command contains 5 bytes:
+  // 	First byte is the stage number: 1 or 2
+  //	Second byte is the direction: 1 or 2
+  //	Third - Fifth bytes are number of steps: 000 - 999
+  if (command[0]!=0)
+  {
+    // Check the first byte and if it is not '1' or '2' discard it
+    // First byte determines the stage (stepper motor) that needs to be moved
+    if (command[0]!='1' || command[0]!= '2') flag = 0;
+
+    // Check the second byte and if it is not '1' or '2' discard it
+    // Second byte determines the direction
+    if (command[1]!='1' || command[1]!= '2') flag = 0;
+
+    // Check that third to fifth bytes are between '0' and '9'
+    // make sure to convert from chars to integers (subtract 48, the ASCII constant) and multiply accordingly
+    for (int i=2; i<5; i++){
+      if ( (command[i]-48) < 0 || (command[i]-48) > 9 ) flag = 0;
+    }
+
+
+    motor =command[0]-48;
+    direction = command[1]-48;
+    steps = (command[2]-48)*100+(command[3]-48)*10+(command[4]-48);
+
+
+    // If everything is fine, move the motors
+    if (flag){
+      check = move_steps (command[0], command[1], steps);
+    }
+
+
+    // Check is sent over the serial back to microprocessor:
+    //     '0' if motor moved
+    //     '1' if switch 1 is pressed
+    //     '2' if switch 2 is pressed
+    //     '3' if switch 3 is pressed
+    //     '4' if switch 4 is pressed
+    //     '5' if command is bad
+    Serial.print(check);
+
+    // Reset the command
+    command[0] = '\0';
+    Serial.flush();
+  }
+
+  // Delay in ms
+  delay(5);
+}
---
+++ Lab06/skeleton/mP/pid.h
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include "iostream"
+#include <sys/time.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/legacy/legacy.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <stdio.h>    // Standard input/output definitions 
+#include <unistd.h>   // UNIX standard function definitions 
+#include <fcntl.h>    // File control definitions 
+#include <errno.h>    // Error number definitions 
+#include <termios.h>  // POSIX terminal control definitions 
+#include <string.h>   // String function definitions 
+
+/*
+Function serialport_init initializes a serial port for communication.
+Inputs:
+const char* serialport - path to input port e.g. "/dev/ttymxc3"
+int baud - baud rate, speed of communication
+*/
+int serialport_init(const char* serialport, int baud);
+
+/*
+Function serialport_close closes serial port communication.
+Inputs:
+int fd - file descriptor
+*/
+int serialport_close(int fd);
+
+/*
+Function serialport_write writes to a serial port.
+Inputs:
+int fd - file descriptor
+const char* str - command to be written to port
+*/
+int serialport_write(int fd, const char* str);
+
+/*
+Function serialport_read_until reads a serial port.
+Inputs:
+int fd - file descriptor
+char* buf - buffer that we read into
+char until - end condition for read function
+int buf_max - maximum buffer length
+int timeout - timeout in seconds
+*/
+int serialport_read_until(int fd, char* buf, char until, int buf_max, int timeout);
+
+/*
+Function CrossTarget overlays a cross at a certain target position, on a provided image 
+Inputs:
+IplImage* inImg - source image  
+int x - x coordinate of the target
+int y - y coordinate of the target
+int size - half of the size of the cross that is drawn on the provided image (in pixels)
+int line_thickness - thickness of the cross that is drawn on the provided image
+Outputs:
+/
+Return:
+IplImage* outImg - source image with a cross at the specified target position
+*/
+IplImage* CrossTarget (IplImage* inImg, int x, int y, int size, int line_thickness);
+
+/*
+Function ColorTracking detects the center of a specific color in the source image
+Inputs:
+IplImage* inImg - source image  
+int color - if 1, blue color is detected and if 2, yellow color is detected
+Outputs:
+int* positionX - x coordinate of the center of detected color (in pixels)
+int* positionY - y coordinate of the center of detected color (in pixels)
+Return:
+0
+*/
+int ColorTracking (IplImage* img, int* positionX , int* positionY, int color, int* posX , int* posY, int count, int drawTraj);
+
+/*
+Function constructCommand creates command to be sent over serial, for the SAM3x microcontroller 
+Inputs:
+int u - number of steps
+int motor - stage (motor) number that determines the axis
+Outputs:
+char* command - command as a string of 5 bytes
+Return:
+0
+*/
+int constructCommand (char* command, int u, int motor);
+
+/*
+Function MoveMotor moves the motor for a specified number of steps
+Inputs:
+int fd - serial port index
+float distance - distance to be moved (in mm). It has to be <5 mm (999 steps - 3 byte limitation)
+int motor - motor to be moved
+Outputs:
+/
+Return:
+1 - if switch is pressed
+-1 - if there is a problem with communication or distance is too big
+0 - otherwise
+
+Notes:
+- Check the pixel - mm relation
+*/
+int MoveMotor (int fd, float distance, int motor);
+
+/*
+Function MoveMotorRectangular moves the stages in a rectangle trajectory with specified distance and
+number of steps per each rectangle side. Additionally, if camera is used, coordinates in each steps
+are written in a .txt file.
+Inputs:
+int fd - serial port index
+float distance - length of each rectangle side (in mm). Each step has to be <5 mm (999 steps - 3 byte limitation) 
+int steps - number of steps that each rectangle side is divided in
+int useVision - if 0, vision is not used, otherwise yes
+int camIndex - system camera index (if vision is not used, it is not considered so it can be any number)
+Outputs:
+/
+Return:
+-1 - if there was an error
+0 - otherwise
+
+Notes:
+- Adjust what color to track
+- Adjust what motor goes first (1 or 2) and in which direction (+ or -)
+*/
+int MoveMotorRectangular (int fd, float distance, int steps, int useVision, int camIndex);
+
+/*
+Function PID uses a color tracker to control the movement of the stages. It can only move one stage 
+at a time. 
+Inputs:
+int fd - serial port index
+int targetPosition - coordinate of the target (in pixels)
+CvCapture* capture - camera index
+int motor - motor to be moved (1 or 2), determines the axis
+Outputs:
+/
+Return:
+-1 - if there was an error
+0 - otherwise
+
+Notes:
+- Target in pixels or mm?
+*/
+
+int PID (int fd, int targetPositionX, int targetPositionY, CvCapture* capture);
+
