a5fec365f5df934b125b98729e7e03a8a1ad74f6
Guillem <gtorrent@everis.com>
2018-12-15 17:10:08 +0100

    Implementing last changes from side repo

--- lib-visualodo/src/duckietown_visualodo/algo/visual_odometry.py
+++ lib-visualodo/src/duckietown_visualodo/algo/visual_odometry.py
@@ -194,32 +194,46 @@ class VisualOdometry:
 
             # Split between far-region and close region matches
             # TODO: improve mask -> make it a function of intrinsic camera calibration / estimated depth
-            proximity_r = ((0.2 * h) ** 2 + (0.5 * w) ** 2) / (0.4 * h)
-            proximity_mask = create_circular_mask(h, w, center=(w / 2, 0.3 * h + proximity_r), radius=proximity_r)
+            mask_params = [0.5, 0.7, 0.4]
+
+            proximity_r = \
+                (((mask_params[0] - mask_params[1]) * h) ** 2 + (0.5 * w) ** 2) / (
+                    -2 * (mask_params[0] - mask_params[1]) * h)
+            proximity_mask = create_circular_mask(h, w, center=(w / 2, (1 - mask_params[1]) * h + proximity_r),
+                                                  radius=proximity_r)
+            proximity_mask_2 = create_circular_mask(h, w, center=(w / 2, (1 - mask_params[1]) * h),
+                                                    radius=mask_params[2] * w)
+            proximity_mask = ~proximity_mask_2 + proximity_mask
 
             match_distance_filter = DistanceFilter(matched_query_points, matched_train_points, self.camera_K, (h, w))
             match_distance_filter.split_by_distance(proximity_mask)
 
             # Remove me
             if parameters.plot_matches:
-                processed_data_plotter.plot_point_correspondences(
-                    matched_query_points, matched_train_points, proximity_mask)
+                processed_data_plotter.plot_displacements_from_distance_mask(match_distance_filter)
 
-            n_distant_matches = len(match_distance_filter.distant_query_points)
+            n_distant_matches = len(match_distance_filter.norm_distant_query_points)
             rot_hypothesis = []
+
+            # Average sign of rotation
+            rot_sign = np.sign(np.average(np.array(matched_query_points) - np.array(matched_train_points), axis=0)[0])
 
             # Calculate two rotation hypothesis for all far matches assuming that they lay close to the ground plane
             for distant_q_p, distant_t_p in \
-                    zip(match_distance_filter.distant_query_points, match_distance_filter.distant_train_points):
+                    zip(match_distance_filter.norm_distant_query_points,
+                        match_distance_filter.norm_distant_train_points):
 
                 a = (distant_t_p[0] - distant_q_p[0]) \
-                    / np.sqrt((distant_t_p[0]*distant_q_p[0])**2+distant_t_p[0]**2+distant_q_p[0]**2+1)
-                rot = np.arccos((distant_t_p[1]+distant_t_p[0]*distant_q_p[1]*np.array([a, -a]))/distant_q_p[1])
+                    / np.sqrt((distant_t_p[0] * distant_q_p[0]) ** 2 + distant_t_p[0] ** 2 + distant_q_p[0] ** 2 + 1)
+                rot = np.arccos((distant_t_p[1] + distant_t_p[0] * distant_q_p[1] * np.array([a, -a])) / distant_q_p[1])
                 rot = [rot_h for rot_h in rot if not isnan(rot_h)]
 
+                # Use hypothesis whose sign is consistent with the average sign
                 for rot_h in np.unique(rot):
-                    rot_hypothesis.append(rot_h)
-                    rot_hypothesis.append(-rot_h)
+                    if np.sign(rot_h) == rot_sign:
+                        rot_hypothesis.append(-rot_h)
+                    else:
+                        rot_hypothesis.append(rot_h)
 
             rot_hypothesis = np.unique(rot_hypothesis)
             rot_hypothesis_rmse = np.zeros((len(rot_hypothesis), 1))
@@ -232,13 +246,13 @@ class VisualOdometry:
                                     [-np.sin(hypothesis), 0, np.cos(hypothesis)]])
 
                 rotated_train_points = np.matmul(
-                    np.append(np.reshape(match_distance_filter.distant_train_points, (n_distant_matches, 2)),
+                    np.append(np.reshape(match_distance_filter.norm_distant_train_points, (n_distant_matches, 2)),
                               np.ones((n_distant_matches, 1)), axis=1),
                     rot_mat)
 
                 # Calculate rmse of hypothesis with all peripheral points
                 error = rotated_train_points[:, 0:2] - np.reshape(
-                    match_distance_filter.distant_query_points, (n_distant_matches, 2))
+                    match_distance_filter.norm_distant_query_points, (n_distant_matches, 2))
 
                 rot_hypothesis_rmse[hypothesis_index] = np.sum(np.sqrt(np.sum(np.power(error, 2), axis=1)))
 
@@ -258,7 +272,7 @@ class VisualOdometry:
 
             # Estimate translation vector
             for proximal_q_p, proximal_t_p in \
-                    zip(match_distance_filter.close_query_points, match_distance_filter.close_train_points):
+                    zip(match_distance_filter.norm_close_query_points, match_distance_filter.norm_close_train_points):
                 proximal_q_p = np.matmul(np.transpose(z_rot_mat), np.append([proximal_q_p], [1]))
 
                 ty = (proximal_t_p[1] - proximal_q_p[1]) * self.joy_command.v
@@ -277,13 +291,13 @@ class VisualOdometry:
                 h_matrix = np.append(h_matrix, np.expand_dims(np.array([0, 0, 0, 1]).T, axis=0), axis=0)
 
                 transformed_train_points = np.matmul(
-                    np.append(np.reshape(match_distance_filter.close_train_points, (n_proximal_matches, 2)),
+                    np.append(np.reshape(match_distance_filter.norm_close_train_points, (n_proximal_matches, 2)),
                               np.ones((n_proximal_matches, 2)), axis=1),
                     h_matrix)
 
                 # Calculate rmse of hypothesis with all peripheral points
                 error = transformed_train_points[:, 0:2] - np.reshape(
-                    match_distance_filter.close_query_points, (n_proximal_matches, 2))
+                    match_distance_filter.norm_close_query_points, (n_proximal_matches, 2))
                 t_hypothesis_rmse[hypothesis_index] = np.sum(np.sqrt(np.sum(np.power(error, 2), axis=1)))
 
             # t_vec = t_hypothesis[np.argmin(t_hypothesis_rmse)]
